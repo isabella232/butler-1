34239d923933beb0be2a267299374ef7bf6beb88
erikvdbergh <erikvdbergh@gmail.com>
2017-06-06 14:27:00 +0100

    add terraform files for provisioning

---
+++ provision/terraform/vars.tf
@@ -0,0 +1,34 @@
+variable "key_pair" {
+}
+
+variable "image_id" {
+
+}
+
+variable "user" {
+	default = "centos"
+}
+
+variable "key_file" {
+
+}
+
+variable "bastion_key_file" {
+
+}
+
+variable "bastion_host" {
+
+}
+
+variable "bastion_user" {
+	default = "centos"
+}
+	
+variable "main_network_id" {
+
+}
+
+variable "gnos_network_id" {
+
+}	
