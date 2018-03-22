5ee1363e331c989096abe5c0e7a3ca936b900137
erikvdbergh <erikvdbergh@gmail.com>
2018-03-22 14:33:19 +0000

    pin tornado version

--- examples/deployment/openstack/large-cluster/saltmaster.tf
+++ examples/deployment/openstack/large-cluster/saltmaster.tf
@@ -44,7 +44,7 @@ resource "openstack_compute_instance_v2" "salt-master" {
 		     "sudo yum install salt-minion -y",
 		     "sudo yum install python-pip -y",
 		     "sudo pip uninstall tornado -y",
-		     "sudo pip install tornado",
+		     "sudo pip install tornado==4.5.3",
 		     "sudo yum install GitPython -y",
 		     "sudo service salt-master start",
 		     "sudo hostname salt-master"
