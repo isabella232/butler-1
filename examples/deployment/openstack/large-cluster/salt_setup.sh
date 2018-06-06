7b56a5e6cd14edd1268714846320ead8c5094d29
Tony Wildish <wildish@ebi.ac.uk>
2018-06-06 10:54:44 +0000

    Don't assume firewalld is there, it might not be

--- examples/deployment/openstack/large-cluster/salt_setup.sh
+++ examples/deployment/openstack/large-cluster/salt_setup.sh
@@ -2,10 +2,10 @@
 
 echo "got $1 as master ip, $2 as id and $3 as roles"
 
-set -ex
 sudo systemctl disable firewalld
 sudo systemctl stop firewalld
 
+set -ex
 sudo yum install epel-release -y
 #sudo yum -y update
 sudo yum install wget yum-plugin-priorities -y
