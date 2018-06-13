cff44a7cbedc4e539f0b7c003b488606e668a800
Tony Wildish <wildish@ebi.ac.uk>
2018-06-13 15:10:14 +0000

    Set grafana going

--- examples/deployment/openstack/large-cluster/salt-epilogue.sh
+++ examples/deployment/openstack/large-cluster/salt-epilogue.sh
@@ -3,3 +3,5 @@
 set -ex
 sudo salt-run mine.update '*'                 2>&1 | tee salt.mine.update.log
 sudo salt-run state.orchestrate butler.deploy 2>&1 | tee salt.butler.deploy.log
+
+sudo ./setup-grafana.sh
