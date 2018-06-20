fd784b61adcf6f07b6082335def0a8e036ca4593
Tony Wildish <wildish@ebi.ac.uk>
2018-06-20 14:50:31 +0000

    switch from single to double quotes. Why? Well...

--- examples/deployment/openstack/large-cluster/get-worker-ssh-config.sh
+++ examples/deployment/openstack/large-cluster/get-worker-ssh-config.sh
@@ -7,8 +7,8 @@ user=$3
 salt 'worker-*' cmd.run '/sbin/ifconfig eth0' | \
   egrep '^worker|inet ' | \
   sed \
-    -e 's%^\s*inet\s*%%' \
-    -e 's%\s.*$%%' \
-    -e 's%^worker%\nHost worker%' \
-    -e 's%:%%' \
-    -e 's%^1%  ProxyCommand ssh -i ${identity} ${user}@${bastion} -W \%h:\%p\n  Hostname 1%'
+    -e "s%^\s*inet\s*%%" \
+    -e "s%\s.*$%%" \
+    -e "s%^worker%\nHost worker%" \
+    -e "s%:%%" \
+    -e "s%^1%  ProxyCommand ssh -i ${identity} ${user}@${bastion} -W \%h:\%p\n  Hostname 1%"
