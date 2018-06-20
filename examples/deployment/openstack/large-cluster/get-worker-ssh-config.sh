950fc520562d34271905b1a64d15bb9270233452
Tony Wildish <wildish@ebi.ac.uk>
2018-06-20 15:11:24 +0000

    Save the output automatically

--- examples/deployment/openstack/large-cluster/get-worker-ssh-config.sh
+++ examples/deployment/openstack/large-cluster/get-worker-ssh-config.sh
@@ -11,4 +11,5 @@ salt 'worker-*' cmd.run '/sbin/ifconfig eth0' | \
     -e "s%\s.*$%%" \
     -e "s%^worker%\nHost worker%" \
     -e "s%:%%" \
-    -e "s%^1%  ProxyCommand ssh -i ${identity} ${user}@${bastion} -W \%h:\%p\n  Hostname 1%"
+    -e "s%^1%  ProxyCommand ssh -i ${identity} ${user}@${bastion} -W \%h:\%p\n  Hostname 1%" | \
+  tee ssh-config
