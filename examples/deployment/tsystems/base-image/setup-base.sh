7b9629a62049962c20ddcd5aa7d026c1118b240c
Tony Wildish <wildish@ebi.ac.uk>
2018-07-10 14:33:15 +0000

    Add bzip2 to the base image

--- examples/deployment/tsystems/base-image/setup-base.sh
+++ examples/deployment/tsystems/base-image/setup-base.sh
@@ -14,7 +14,7 @@ set -ex
 yum install -y epel-release
 yum -y update
 yum install -y python-pip git python-pygit2 wget yum-plugin-priorities
-yum install -y gcc gcc-c++ make cmake kernel-devel zlib-devel bzip2-devel xz-devel
+yum install -y gcc gcc-c++ make cmake kernel-devel zlib-devel bzip2 bzip2-devel xz-devel
 yum install -y https://repo.saltstack.com/yum/redhat/salt-repo-latest-2.el7.noarch.rpm
 yum install -y salt-minion
 systemctl disable firewalld
