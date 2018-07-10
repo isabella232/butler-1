d550a7e2b320a01871437477f14058690a6c5e2c
Tony Wildish <wildish@ebi.ac.uk>
2018-07-10 12:33:34 +0000

    retry a few ties in case of failures, logic for setting version automatically has been broken by their naming scheme

--- examples/deployment/tsystems/large-cluster/mount-oneclient.sh
+++ examples/deployment/tsystems/large-cluster/mount-oneclient.sh
@@ -6,10 +6,19 @@ if [ "$token" == "" ]; then
   exit 1
 fi
 
-vsn=$2
-if [ "$vsn" == "" ]; then
-  vsn="oneclient-18.02.0.rc8"
-fi
+#
+# This would be nice, but onedata broke the logic :-(
+# Instead of using 18.02.0.rc8, as deduced, I have to
+# use...
+vsn="oneclient-18.02.0.rc8-1.el7.centos.x86_64"
+#
+# Beaurk!
+#
+#vsn=$2
+#if [ "$vsn" == "" ]; then
+#  vsn="18.02.0.rc8"
+#fi
+#vsn="oneclient-${vsn}"
 
 if [ ! -f /usr/bin/oneclient ]; then
   curl -sS -o oneclient.sh http://get.onedata.org/oneclient.sh
@@ -19,14 +28,25 @@ fi
 
 sudo mkdir -p /data
 
-if [ ! -d /data/OTC-EMBL ]; then
-  sudo oneclient -i -H ebi-otc.onedata.hnsc.otc-service.com \
-    -t $token \
-    /data --force-direct-io \
-    -o allow_other \
-    --force-fullblock-read \
-    --rndrd-prefetch-cluster-window=10485760 \
-    --rndrd-prefetch-cluster-block-threshold=5 \
-    --provider-timeout=7200 \
-    -v 1
-fi
+count=3
+while [ $count -gt 0 ];
+do
+  if [ ! -d /data/OTC-EMBL ]; then
+    sudo oneclient -i -H ebi-otc.onedata.hnsc.otc-service.com \
+      -t $token \
+      /data --force-direct-io \
+      -o allow_other \
+      --force-fullblock-read \
+      --rndrd-prefetch-cluster-window=10485760 \
+      --rndrd-prefetch-cluster-block-threshold=5 \
+      --provider-timeout=7200 \
+      -v 1
+  fi
+  if [ -d /data/OTC-EMBL ]; then
+    exit 0
+  fi
+
+  count=`expr $count - 1`
+  echo "Mount failed, trying again in a few seconds"
+  sleep $((RANDOM % 10 + 3 ))
+done
